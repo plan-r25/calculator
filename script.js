f4248f3c309d97c2cf1fece8de34270b4b768d48
planR <rplann24@gmail.com>
2026-02-23 11:58:24 +0000

    working on how to calculate the string

--- script.js
+++ script.js
@@ -5,9 +5,10 @@ const result = document.querySelector("#result");
 const operator = document.querySelector("#operator");
 const number = document.createElement('div');
 
-number.append(operator, div)
+number.append(div)
 
 div.classList.add('buttons');
+div.appendChild(operator);
 
 for (let i = 0; i <= 9; i++) {
   const num = document.createElement('button');
@@ -21,11 +22,21 @@ container.appendChild(number);
 const buttons = document.querySelectorAll(".buttons button");
 buttons.forEach(button => {
   button.addEventListener("click", (e) => {
+    calc.textContent += e.target.textContent;
+    let parts = calc.textContent.split(/[+\-*/]/);
+    console.log('parts', parts);
+    let a = +parts[0];
+    let op = parts[1]
+    let b = +parts[2]
+    console.log('part A',a)
+    
     console.log(e.target.textContent);
-    calc.textContent = add(e.target.textContent, e.target.textContent);
+    if (op === '*') return a * b;
+    if (op === '+') return a + b;
+    if (op === '-') return a - b;
+    if (op === '/') return a / b;
+    let input = `${a} ${op} ${b}`
+    result.textContent = input ;
+    console.log('part B', b)
   })
-})
-
-function add(a, b) {
-  return a + b;
-}
+});
