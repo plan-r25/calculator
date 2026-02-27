539d5390a6dcebacf3d712c56ffdb2a6578e31bd
planR <rplann24@gmail.com>
2026-02-27 11:52:58 +0000

    making the calculation more robust

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const dlt = document.querySelector("#delete");
 const clear = document.querySelector("#clear");
 const equal = document.querySelector("#equal");
 
+
 //OR
 let input ='';
 buttons.forEach(button => {
@@ -13,10 +14,22 @@ buttons.forEach(button => {
     button.id === 'equal' ||
     button.id === 'clear'
   ) return; 
+  const isOperator = /[+\-*/%]/;
   button.addEventListener("click", () => {
-    input += button.textContent;
+    const value = button.textContent;
+    if (
+     isOperator.test(value) &&
+     isOperator.test(input.slice(-1))     
+    ) {
+      return;
+    }
+    input += value;
     console.log('clicked', button.textContent);
     calc.textContent = input;
+
+    calc.classList.remove("small");
+    result.classList.remove("large");
+    updateResult();
   })
 });
 dlt.addEventListener("click", () => {
@@ -27,10 +40,13 @@ dlt.addEventListener("click", () => {
 clear.addEventListener("click", () => {
   input = input.slice(0, -1);
   calc.textContent = input;
-  result.textContent = input;
+
+  updateResult();
 });
 equal.addEventListener("click", () => {
   result.textContent = calculate(input);
+  calc.classList.add("small");
+  result.classList.add("large");
 });
 
 function calculate(str) {
@@ -47,3 +63,21 @@ function calculate(str) {
   }, 0)
   return res;
 };
+
+function updateResult() {
+  // empty input
+  if (!input) {
+    result.textContent = "";
+    return;
+  }
+  if (input.length < 3) {
+    result.textContent = "";
+    return;
+  }
+  // calculate
+  if (/[+\-*/%0-9]$/.test(input)) {
+    result.textContent = calculate(input);
+    return;
+  }
+  result.textContent = calculate(input);
+}
